32bc4646202dd89d4295b7745697e8781f2d2487
Swetha <swetha00200@gmail.com>
2026-03-06 23:45:24 +0530

    feat: added favicon,  cart and wishlist quantity and centered footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mt-2">
-      <div className="text-light py-2 ">
+      <div className="text-light py-2 text-center">
         <p className="container ">&copy; 2025 | Furnicart</p>
       </div>
     </footer>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,17 +2,17 @@ import { Link, useNavigate } from "react-router-dom";
 import { BsFillHeartFill } from "react-icons/bs";
 import { BsCart3 } from "react-icons/bs";
 import { BsFillPersonFill } from "react-icons/bs";
-import useFurnitureContext from "../context/FurnitureContext";
 import { useState } from "react";
+import useCartContext from "../context/CartContext";
+import useFurnitureContext from "../context/FurnitureContext";
 export default function Nav() {
-  // const { products, setFilteredProducts } = useFurnitureContext();
-  const navigate = useNavigate()
-  const [searchValue,setSearchValue] = useState("")
-
-const searchHandler = () => {
-  navigate(`/products?search=${searchValue}`);
-};
-
+  const navigate = useNavigate();
+  const [searchValue, setSearchValue] = useState("");
+  const { cartProducts } = useCartContext();
+  const {wishlistProducts} = useFurnitureContext()
+  const searchHandler = () => {
+    navigate(`/products?search=${searchValue}`);
+  };
 
   return (
     <div>
@@ -21,29 +21,36 @@ const searchHandler = () => {
           <Link to="/" className="nav-brand">
             FurniCart
           </Link>
-         <div className="d-flex">
-           <input
-            className="form-control mx-2"
-            style={{width:"250px"}}
-            type="search"
-            placeholder="Search for products"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-          />
-          <button className="btn btn-light" onClick={searchHandler}>Search</button>
-
-         </div>
+          <div className="d-flex">
+            <input
+              className="form-control mx-2"
+              style={{ width: "250px" }}
+              type="search"
+              placeholder="Search for products"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+            <button className="btn btn-light" onClick={searchHandler}>
+              Search
+            </button>
+          </div>
           <div className="d-flex">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link to={"/wishList"} className="nav-icon">
-                  {" "}
                   <BsFillHeartFill />
+                 <span className="nav-count">
+                    ({wishlistProducts.length})
+                  </span>
                 </Link>
+                 
               </li>
               <li className="nav-item">
                 <Link to={"/cart"} className="nav-icon">
                   <BsCart3 />
+                  <span className=" nav-count">
+                    ({cartProducts.length})
+                  </span>
                 </Link>
               </li>
               <li className="nav-item">
