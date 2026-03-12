a3c98acdc3c3ac48ac8f50d1a61a16441acd725b
Swetha <swetha00200@gmail.com>
2026-03-12 18:15:06 +0530

    fix: Footer fixed at bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="mt-2">
+    <footer className="mt-2 fixed-bottom ">
       <div className="text-light py-2 text-center">
-        <p className="container ">&copy; 2025 | Furnicart</p>
+        <p>&copy; 2025 | Furnicart</p>
       </div>
     </footer>
   );
